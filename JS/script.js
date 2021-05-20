a4f4ff7057c3f0985a2d9fa9c973312e701ad48b
Tunde Samson <odewoletundesamson@gmail.com>
2021-05-20 15:58:58 +0100

    update commit

--- JS/script.js
+++ JS/script.js
@@ -302,6 +302,32 @@ window.addEventListener("load", ()=>{
 })
 
 
+// slide up btn
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         // sticky navbar on scroll script
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     // slide up btn
+//         if(this.scroll > 500){
+//             $('.scroll-up-btn').addClass('show')
+//         }
+//         else{
+//             $('.scroll-up-btn').removeClass('show')
+//         }
+//     })
+// })
+
+// // slide up btn script
+// $('.scroll-up-btn').click(function(){
+//     $('html').animate({scrollTop: 0});
+//     // removing smooth scroll on slide-up button click
+//     $('html').css("scrollBehavior", "auto");
+// });
+
 // typing text animation script
 var typed = new Typed(".typing", {
     strings: ["YouTuber.", "Developer.", "Blogger.", "Designer.", "Freelancer."],
